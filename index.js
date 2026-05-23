6d91b55c14689d225b2d396741dcab8d4f20128b
kaanyilmaz3 <kaanbrawler6@gmail.com>
2026-05-23 13:07:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ const client = new Client({
 client.once('ready', () => {
   console.log('Bot aktif!');
 });
+const channel = await client.channels.fetch('1507495583563386910');
+
+channel.send(`
+📃│rules
+
+1- 
+2- Spam yasak
+3- Saygılı olun
+`);
 
 client.login(process.env.TOKEN);
